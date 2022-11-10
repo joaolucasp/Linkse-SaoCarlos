b43e661fa7184c7bf0bdfbe370a3d0e5b6c4d1f8
João Lucas Pereira <lucaaspereeira@gmail.com>
2022-11-10 01:20:45 -0300

    aperfeiçoamentos na navbar (navbar toggler collapsed modo claro e modo escuro) e aperfeiçoamentos no efeito hover dos itens da navbar

---
+++ script/navbar.js
@@ -0,0 +1,42 @@
+const buttonHamburguer = document.getElementsByClassName("navbar-toggler");
+var header = document.getElementById("mainNavBar")
+
+window.addEventListener("scroll", function (){
+    if(header.classList.contains("collapsed-background")) return;
+
+    stickyFunction();
+});
+
+function stickyFunction() {
+    header.classList.toggle("sticky", window.scrollY > 0);
+
+    let logo = document.getElementById("logoMain");
+    let logoSecondary = document.getElementById("logoSecondary");
+
+    if(this.window.scrollY == 0){
+        logo.classList.remove("d-none");
+        logoSecondary.classList.add("d-none");
+    } else {
+        logo.classList.add("d-none");
+        logoSecondary.classList.remove("d-none");
+    }
+}
+
+buttonHamburguer = window.addEventListener('click', event => {
+    // Disable Dark Navbar
+    if(header.classList.contains("open-collapsed")){
+        header.classList.remove("open-collapsed")
+
+        header.classList.remove("collapsed-background");
+        stickyFunction();
+        return
+    }
+
+    //Enable Dark Navbar
+    header.classList.add("open-collapsed");
+
+    if(!(header.classList.contains("collapsed-background")) && window.scrollY == 0 ){
+        header.classList.add("collapsed-background");
+    }
+ })
+
